fcc754f67cdddb4f924634af19be6727a345812e
sagesaint84 <sagesaint84@gmail.com>
2026-08-31 17:33:30 +0900

    v5.1.4: 백엔드 서버 데몬 리로더 정밀 재기동 및 자녀/엄마 비공제 1,800만 완벽 동기화 완료

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,5 +1,5 @@
 // Wealth Service Worker - MDN Progressive Web App Standard
-const CACHE_NAME = "wealth-cache-v62";
+const CACHE_NAME = "wealth-cache-v64";
 const PRECACHE_RESOURCES = [
   "/",
   "/dashboard",
