1efe80bb57697dbc0b27e9d01b7f5e0897405b92
sagesaint84 <sagesaint84@gmail.com>
2026-09-02 23:45:36 +0900

    chore: SemVer 패치 버전 넘버링(+1 단위 순차 증가) 정렬 및 CHANGELOG 동기화

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,5 +1,5 @@
 // Wealth Service Worker - MDN Progressive Web App Standard
-const CACHE_NAME = "wealth-cache-v108";
+const CACHE_NAME = "wealth-cache-v106";
 const PRECACHE_RESOURCES = [
   "/",
   "/dashboard",
